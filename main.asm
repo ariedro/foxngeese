d360a4febd76bc91e5a2b5e6b06a118c89dddb4e
ariedro <ariedro@gmail.com>
2024-06-22 16:36:51 -0300

    add turns

--- main.asm
+++ main.asm
@@ -19,6 +19,7 @@ section .data
   posFox    db  4,5
   posGeese  db  3,1,  4,1,  5,1,  3,2,  4,2,  5,2,  1,3,  2,3,  3,3,  4,4,  5,3,  6,3,  7,3,  1,4,  7,4,  1,5,  7,5
   msgInChar db  "insert a character",0
+  turn      db  1 ; 1 - fox, 2 - geese
 
 section .bss
   text            resb 500
@@ -39,6 +40,25 @@ main_loop:
   mPuts
   mov     rdi, text
   mGets
+
+  ; check turn
+  mov     rax, 0
+  mov     al, [turn]
+  cmp     al, 1
+  je      fox_turn
+
+geese_turn:
+  ; update next turn
+  dec     al
+  mov     [turn],al
+
+  ; TODO: implement geese movement
+  jmp     main_loop
+
+fox_turn:
+  ; update next turn
+  inc     al
+  mov     [turn],al
 
   ; call movement process
   mov     rdi, posFox
