cf07379a0e2f2161ecf4092eee3faec84f79b1dc
ariedro <ariedro@gmail.com>
2024-06-28 17:08:58 -0300

    fix geese original coords

--- main.asm
+++ main.asm
@@ -9,7 +9,7 @@ extern selectGoose
 
 section .data
   posFox            db  4,5
-  posGeese          db  3,1,  4,1,  5,1,  3,2,  4,2,  5,2,  1,3,  2,3,  3,3,  4,4,  5,3,  6,3,  7,3,  1,4,  7,4,  1,5,  7,5
+  posGeese          db  3,1,  4,1,  5,1,  3,2,  4,2,  5,2,  1,3,  2,3,  3,3,  4,3,  5,3,  6,3,  7,3,  1,4,  7,4,  1,5,  7,5
   turn              db  1 ; 1 - fox, 2 - geese
   selectedGoose     db  1
   selectingGoose    db  0 ; 0 - no, 1 - yes
